b1cb95a85b854019447c6b2b380dc59eb60869c8
Renato Souza <renatovieiradesouza1@gmail.com>
2019-07-09 23:00:01 -0300

    Update program.py

--- program.py
+++ program.py
@@ -1,4 +1,4 @@
-`def decodificaTexto(textoParaDecifrar,qtdCasasDecifrar):
+def decodificaTexto(textoParaDecifrar,qtdCasasDecifrar):
     
     #Parametros de uso
     alfabeto = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
